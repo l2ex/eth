52c90cd546b663a98e5ff6080bffc4d18c1490e2
devmanio <ivan.kocheshev@gmail.com>
2018-06-26 19:06:40 +0300

    channel withdraw pipeline

--- contracts/l2dex.sol
+++ contracts/l2dex.sol
@@ -2,42 +2,74 @@ pragma solidity ^0.4.19;
 
 contract l2dex{
     address public l2dex;
-    
+    address public oracle;//main oracle
+    enum State { canWithdraw, cantWithdraw }
+    struct Channel {
+        uint256 ttl;
+        uint32  nonce;
+        uint256 sum;
+        State state;
+    }
+    mapping(address => Channel) public channels;
+    event chanelChange(address sender, uint256 ttl, uint256 sum);
+    event withdrawRequest(address sender);
+    event withdrawChanel(address sender, uint256 sum);
     /**
    * @dev Throws if called by any account other than the l2dex.
    */
     modifier onlyOracle(){
-        require(msg.sender == l2dex);
+        require(msg.sender == oracle);
         _;
     }
     /**
    * @dev Constructor, initial l2dex oracle address.
    */
-    function l2dex(){
+    function l2dex(address _oracle){
         l2dex = msg.sender;
+        oracle = _oracle;
     }
+    /**
+   * @dev change l2dex address.
+   */
+   function changeL2dex(address _new) onlyOracle{
+       l2dex = _new;
+   }
     /**
    * @dev Open or deposit channel by user
    */
-    function openChanel() public payable{
+    function openChanel(uint256 _ttl) public payable{
+        channels[msg.sender].ttl = now+_ttl;
+        channels[msg.sender].state = State.cantWithdraw;
+        if (channels[msg.sender].nonce>0) { //exist
+            channels[msg.sender].sum += msg.value;
+        } 
+        else{  //new
+            channels[msg.sender].sum = msg.value;
+            channels[msg.sender].nonce = 0;
+        }
+        emit chanelChange(msg.sender, channels[msg.sender].ttl, channels[msg.sender].sum );
         
     }
     /**
-   * @dev Etend ttl by user
+   * @dev Extend ttl by user
    */
-   function updateTtl() public{
-       
+   function updateTtl(uint256 _ttl) public{
+       channels[msg.sender].ttl=now+_ttl;
    }
    /**
    * @dev Request withdraw by user
    */
    function withdrawReq() public{
-       
+       emit withdrawRequest(msg.sender);
    }
    /**
    * @dev withdraw by user
    */
    function withdraw() public{
+       require(channels[msg.sender].state == State.canWithdraw);
+       msg.sender.transfer(channels[msg.sender].sum);
+       channels[msg.sender].sum = 0;
+       emit withdrawChanel(msg.sender, channels[msg.sender].sum);
        
    }
    /**
