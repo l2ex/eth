1bf116af582d78de45f641c64dd0d71f5af3b141
Igor Sokolov <wardencliffe.sis@gmail.com>
2018-06-29 11:42:47 +0300

    Supported ERC20 tokens in l2dex contract. Some refactoring is done.

--- contracts/l2dex.sol
+++ contracts/l2dex.sol
@@ -1,102 +1,182 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.24;
 
-contract l2dex{
-    address public l2dex;
-    address public oracle;//main oracle
-    enum State { canWithdraw, cantWithdraw }
-    struct Channel {
-        uint256 ttl;
-        uint32  nonce;
-        uint256 sum;
-        State state;
-    }
-    mapping(address => Channel) public channels;
-    event chanelChange(address sender, uint256 ttl, uint256 sum);
-    event withdrawRequest(address sender);
-    event withdrawChanel(address sender, uint256 sum);
-    /**
-   * @dev Throws if called by any account other than the l2dex.
-   */
-    modifier onlyOracle(){
-        require(msg.sender == oracle);
-        _;
-    }
-    /**
-   * @dev Throws if called by any account other than the l2dex.
-   */
-    modifier onlyL2dex(){
-        require(msg.sender == l2dex);
-        _;
-    }
-    /**
-   * @dev Constructor, initial l2dex oracle address.
-   */
-    function l2dex(address _oracle){
-        l2dex = msg.sender;
-        oracle = _oracle;
-    }
-    /**
-   * @dev change l2dex address.
-   */
-   function changeL2dex(address _new) onlyOracle{
-       l2dex = _new;
-   }
-    /**
-   * @dev Open or deposit channel by user
-   */
-    function openChanel(uint256 _ttl) public payable{
-        channels[msg.sender].ttl = now+_ttl;
-        channels[msg.sender].state = State.cantWithdraw;
-        if (channels[msg.sender].nonce>0) { //exist
-            channels[msg.sender].sum += msg.value;
-        } 
-        else{  //new
-            channels[msg.sender].sum = msg.value;
-            channels[msg.sender].nonce = 1;
-        }
-        emit chanelChange(msg.sender, channels[msg.sender].ttl, channels[msg.sender].sum );
-        
-    }
-    /**
-   * @dev Extend ttl by user
-   */
-   function updateTtl(uint256 _ttl) public{
-       channels[msg.sender].ttl=now+_ttl;
-   }
-   /**
-   * @dev Request withdraw by user
-   */
-   function withdrawReq() public{
-       emit withdrawRequest(msg.sender);
-   }
-   /**
-   * @dev withdraw by user
-   */
-   function withdraw() public{
-       require(channels[msg.sender].state == State.canWithdraw);
-       msg.sender.transfer(channels[msg.sender].sum);
-       channels[msg.sender].sum = 0;
-       emit withdrawChanel(msg.sender, channels[msg.sender].sum);
-       
-   }
-   /**
-   * @dev Push last tx 
-   */
-   function pushTx(address _channelOwner, uint32 _id, uint256 _sum, uint8 v,bytes32 r,bytes32 s) public{
-       require(_id>channels[_channelOwner].nonce);
-       //require(channels[_channelOwner].sum > 0);
-       if (_channelOwner == ecrecover(keccak256(_channelOwner,_id,_sum), v, r, s)) {
-           require(channels[_channelOwner].sum >= _sum);
-                  channels[_channelOwner].sum = _sum;
-                  channels[_channelOwner].nonce = _id;
-       } else if (l2dex == ecrecover(keccak256(_channelOwner,_id,_sum), v, r, s)) {
-           require(channels[_channelOwner].sum <= _sum);
-                  channels[_channelOwner].sum = _sum;
-                 channels[_channelOwner].nonce = _id;
-                 channels[_channelOwner].state = State.canWithdraw;
-       }
+import './common/ERC20.sol';
+import './common/SafeMath.sol';
 
-       
-   }
 
+contract l2dex {
+  using SafeMath for uint256;
+
+  enum State { CanWithdraw, CantWithdraw }
+
+  struct Channel {
+    uint256 expiration; // Expiration date (timestamp)
+    uint256 amount;     // Amount of either ether (in weis) or ERC20 token on the channel
+    uint32  nonce;      // Index of the last transaction
+    State   state;      // State of the channel
+  }
+
+
+  uint32 constant DEFAULT_TTL = 3 days;
+
+  address public owner;
+  address public oracle;
+
+  mapping(address => mapping(address => Channel)) public channels;
+
+
+  event ChannelUpdate(address indexed channelOwner, address indexed token, uint256 expiration, uint256 amount, uint32 nonce);
+  event WithdrawRequest(address indexed channelOwner, address indexed token);
+  event Withdraw(address indexed channelOwner, address indexed token, uint256 amount);
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the oracle.
+   */
+  modifier onlyOracle() {
+    require(msg.sender == oracle);
+    _;
+  }
+
+  /**
+   * @dev Throws if channel cannot be withdrawn.
+   */
+  modifier canWithdraw(address token) {
+    require(channels[msg.sender][token].amount > 0);
+    require(channels[msg.sender][token].state == State.CanWithdraw ||
+      now >= channels[msg.sender][token].expiration);
+    _;
+  }
+
+
+  /**
+   * @dev Constructor sets initial owner and oracle addresses.
+   */
+  constructor(address _oracle) public {
+    owner = msg.sender;
+    oracle = _oracle;
+  }
+
+  /**
+   * @dev Changes owner address by oracle.
+   */
+  function changeOwner(address newOwner) public onlyOracle {
+    require(newOwner != address(0));
+    owner = newOwner;
+  }
+
+  /**
+   * @dev Deposits ether to a channel by user.
+   */
+  function deposit() public payable {
+    channels[msg.sender][address(0)].expiration = now.add(DEFAULT_TTL);
+    channels[msg.sender][address(0)].amount = channels[msg.sender][address(0)].amount.add(msg.value);
+    channels[msg.sender][address(0)].nonce += 1;
+    channels[msg.sender][address(0)].state = State.CantWithdraw;
+    emit ChannelUpdate(msg.sender, address(0), channels[msg.sender][address(0)].expiration,
+      channels[msg.sender][address(0)].amount, channels[msg.sender][address(0)].nonce);
+  }
+
+  /**
+   * @dev Deposits ERC20 tokens to a channel by user.
+   */
+  function deposit(address token, uint256 amount) public {
+    // Transfer ERC20 tokens from the sender to the contract and check result
+    require(ERC20(token).transferFrom(msg.sender, this, amount));
+    channels[msg.sender][token].expiration = now.add(DEFAULT_TTL);
+    channels[msg.sender][token].amount = channels[msg.sender][token].amount.add(amount);
+    channels[msg.sender][token].nonce += 1;
+    channels[msg.sender][token].state = State.CantWithdraw;
+    emit ChannelUpdate(msg.sender, token, channels[msg.sender][token].expiration,
+      channels[msg.sender][token].amount, channels[msg.sender][token].nonce);
+  }
+
+  /**
+   * @dev Requests withdraw by user (for ether only).
+   */
+  function requestWithdraw() public {
+    requestWithdraw(address(0));
+  }
+
+  /**
+   * @dev Requests withdraw by user.
+   */
+  function requestWithdraw(address token) public {
+    emit WithdrawRequest(msg.sender, token);
+  }
+
+  /**
+   * @dev Performs withdraw ether to user.
+   */
+  function withdraw() public canWithdraw(address(0)) {
+    msg.sender.transfer(channels[msg.sender][address(0)].amount);
+    channels[msg.sender][address(0)].amount = 0;
+    emit Withdraw(msg.sender, address(0), channels[msg.sender][address(0)].amount);
+  }
+
+  /**
+   * @dev Performs withdraw ERC20 token to user.
+   */
+  function withdraw(address token) public canWithdraw(token) {
+    require(ERC20(token).transfer(msg.sender, channels[msg.sender][token].amount));
+    channels[msg.sender][token].amount = 0;
+    emit Withdraw(msg.sender, token, channels[msg.sender][token].amount);
+  }
+
+  /**
+   * @dev Updates channel with most recent amount by user or by contract owner (for ether only).
+   */
+  function updateChannel(address channelOwner, uint32 nonce, uint256 amount, uint8 v, bytes32 r, bytes32 s) public {
+    updateChannel(channelOwner, address(0), nonce, amount, v, r, s);
+  }
+
+  /**
+   * @dev Updates channel with most recent amount by user or by contract owner.
+   */
+  function updateChannel(address channelOwner, address token, uint32 nonce, uint256 amount, uint8 v, bytes32 r, bytes32 s) public {
+    require(channels[channelOwner][token].nonce > 0 && nonce == channels[channelOwner][token].nonce + 1);
+    address recoveredOwner = ecrecover(keccak256(abi.encodePacked(channelOwner, nonce, amount)), v, r, s);
+    if (recoveredOwner == channelOwner) {
+      // Transaction from user who owns the channel
+      require(amount < channels[channelOwner][token].amount);
+    } else if (recoveredOwner == owner) {
+      // Transaction from the contract owner
+      require(amount > channels[channelOwner][token].amount);
+      channels[channelOwner][token].state = State.CantWithdraw;
+    } else {
+      // Specified arguments are not valid
+      revert();
+    }
+    channels[channelOwner][token].amount = amount;
+    channels[channelOwner][token].nonce = nonce;
+    emit ChannelUpdate(channelOwner, token, channels[channelOwner][token].expiration,
+      channels[channelOwner][token].amount, channels[channelOwner][token].nonce);
+  }
+
+  /**
+   * @dev Extends expiration of the channel by user (for ether only).
+   */
+  function extendChannel(uint256 ttl) public {
+    extendChannel(address(0), ttl);
+  }
+
+  /**
+   * @dev Extends expiration of the channel by user.
+   */
+  function extendChannel(address token, uint256 ttl) public {
+    require(channels[msg.sender][token].nonce > 0);
+    uint256 expiration = now.add(ttl);
+    require(channels[msg.sender][token].expiration <= expiration);
+    channels[msg.sender][token].expiration = expiration;
+    emit ChannelUpdate(msg.sender, token, channels[msg.sender][token].expiration,
+      channels[msg.sender][token].amount, channels[msg.sender][token].nonce);
+  }
 }
