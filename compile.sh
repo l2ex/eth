9c3ab99b7224bf5f7058b9c26644406d6a6fbedc
Igor Sokolov <wardencliffe.sis@gmail.com>
2019-01-31 11:49:49 +0300

    Updated compile script.

--- compile.sh
+++ compile.sh
@@ -17,3 +17,44 @@ solc $SOLC_OPTIONS_ETH contracts/L2DexEthereum.sol
 
 solc $SOLC_OPTIONS_QTUM contracts/common/QRC20Token.sol
 solc $SOLC_OPTIONS_QTUM contracts/L2DexQTUM.sol
+
+mkdir -p ${OUTPUT_PATH}/go/ethereum/erc20
+mkdir -p ${OUTPUT_PATH}/go/ethereum/l2dex
+mkdir -p ${OUTPUT_PATH}/go/qtum/qrc20
+mkdir -p ${OUTPUT_PATH}/go/qtum/l2dex
+
+abigen \
+    --abi ${OUTPUT_PATH}/ERC20Token.abi \
+    --bin ${OUTPUT_PATH}/ERC20Token.bin \
+    --pkg erc20 \
+    --type ERC20Token \
+    --out ${OUTPUT_PATH}/go/ethereum/erc20/token.go
+abigen \
+    --abi ${OUTPUT_PATH}/QRC20Token.abi \
+    --bin ${OUTPUT_PATH}/QRC20Token.bin \
+    --pkg qrc20 \
+    --type QRC20Token \
+    --out ${OUTPUT_PATH}/go/qtum/qrc20/token.go
+    
+abigen \
+    --abi ${OUTPUT_PATH}/L2DexEthereum.abi \
+    --bin ${OUTPUT_PATH}/L2DexEthereum.bin \
+    --pkg l2dex \
+    --type L2DexEthereum \
+    --out ${OUTPUT_PATH}/go/ethereum/l2dex/ethereum.go
+abigen \
+    --abi ${OUTPUT_PATH}/L2DexQTUM.abi \
+    --bin ${OUTPUT_PATH}/L2DexQTUM.bin \
+    --pkg l2dex \
+    --type L2DexQTUM \
+    --out ${OUTPUT_PATH}/go/qtum/l2dex/l2dex.go
+
+cp ${OUTPUT_PATH}/ERC20Token.abi ${OUTPUT_PATH}/go/ethereum/erc20/token.abi
+cp ${OUTPUT_PATH}/ERC20Token.bin ${OUTPUT_PATH}/go/ethereum/erc20/token.bin
+cp ${OUTPUT_PATH}/QRC20Token.abi ${OUTPUT_PATH}/go/qtum/qrc20/token.abi
+cp ${OUTPUT_PATH}/QRC20Token.bin ${OUTPUT_PATH}/go/qtum/qrc20/token.bin
+
+cp ${OUTPUT_PATH}/L2DexEthereum.abi ${OUTPUT_PATH}/go/ethereum/l2dex/ethereum.abi
+cp ${OUTPUT_PATH}/L2DexEthereum.bin ${OUTPUT_PATH}/go/ethereum/l2dex/ethereum.bin
+cp ${OUTPUT_PATH}/L2DexQTUM.abi ${OUTPUT_PATH}/go/qtum/l2dex/l2dex.abi
+cp ${OUTPUT_PATH}/L2DexQTUM.bin ${OUTPUT_PATH}/go/qtum/l2dex/l2dex.bin
